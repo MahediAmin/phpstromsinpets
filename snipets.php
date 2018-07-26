23c3d15fe8c552aa5dff995110ea41e3a50076f9
mahedi <mahedi_cse@hotmail.com>
2018-07-26 20:32:31 +0600

    after_setup_theme_snipets

--- snipets.php
+++ snipets.php
@@ -1,3 +1,22 @@
+<?php
+
 // php start
 [php]
 
+// after_setup_theme_function with parametter
+[afc]
+
+/*
+ * Demo Theme Bootstraping
+ *
+ */
+
+function demoTheme_bootstraping(){
+
+    load_textdomain(demoThem);
+    add_theme_support("post-thumbnails");
+    add_theme_support("title-tag");
+
+
+}
+add_action("after_setup_theme", "demoTheme_bootstraping");
