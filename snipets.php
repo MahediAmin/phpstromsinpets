6db2e736110858859ca91be989be0ce08e876eac
mahedi <mahedi_cse@hotmail.com>
2018-08-10 05:53:49 +0600

    sidebar widget snipets

--- snipets.php
+++ snipets.php
@@ -264,3 +264,17 @@ if ($philosophy_next_post) :
 <?php
 endif;
 ?>
+
+
+// sidebar 
+[sidebar]
+ register_sidebar( array(
+        'name' => __( 'About Us Page', 'philosophy' ),
+        'id' => 'about-us',
+        'description' => __( 'Widgets in this area will be shown on about us page.', 'philosophy' ),
+        'before_widget' => '<div id="%1$s" class="col-block %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="quarter-top-margin">',
+        'after_title'   => '</h3>',
+    ) );
+    
